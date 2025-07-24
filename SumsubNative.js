672536c484197b45538597f62b18c3439767a17b
rayho <rayho.ogglobal.net>
2025-07-24 13:50:06 +0800

    btn color

--- SumsubNative.js
+++ SumsubNative.js
@@ -18,7 +18,7 @@ const sumsubSDK = async (res = {}, tokenPromise = () => { return Promise.resolve
             bottomSheetBackground: getColor("#ffffff", "#333C4D"),
             bottomSheetHandle: getColor("#D1D6E1", "#FFFFFF"),
             backgroundNeutral: getColor("#EDEDED", "#2A2E39"),
-      
+
             // === 文字 ===
             contentStrong: getColor("#333C4D", "#FFFFFF"),
             contentWeak: getColor("#94A0B8", "#999999"),
@@ -27,17 +27,17 @@ const sumsubSDK = async (res = {}, tokenPromise = () => { return Promise.resolve
             contentInfo: getColor("#1693E9", "#4DA6FF"),
       
             // === 欄位 ===
-            fieldBackground: getColor("#F6F7F9", "#2A2E39"),
+            fieldBackground: getColor("#EDEDED", "#2A2E39"),
             fieldContent: getColor("#333C4D", "#FFFFFF"),
             fieldPlaceholder: getColor("#94A0B8", "#999999"),
             fieldTint: getColor("#1693E9", "#4DA6FF"),
       
             // === Primary 按鈕 ===
-            primaryButtonBackground: getColor("#1693E9", "#1485D1"),
+            primaryButtonBackground: getColor("#00A826", "#00A826"),
             primaryButtonContent: getColor("#FFFFFF", "#FFFFFF"),
-            primaryButtonBackgroundDisabled: getColor("#A1D2F7", "#444C5A"),
-            primaryButtonContentDisabled: getColor("#FFFFFF", "#888888"),
-            primaryButtonBackgroundHighlighted: getColor("#1485D1", "#0B4A75"),
+            primaryButtonBackgroundDisabled: getColor("#A6DAB3", "#4C6E57"),
+            primaryButtonContentDisabled: getColor("#444444", "#AAAAAA"),
+            primaryButtonBackgroundHighlighted: getColor("#008F20", "#008F20"),
             primaryButtonContentHighlighted: getColor("#FFFFFF", "#FFFFFF"),
       
             // === Secondary 按鈕 ===
@@ -52,13 +52,11 @@ const sumsubSDK = async (res = {}, tokenPromise = () => { return Promise.resolve
             alertTint: getColor("#1693E9", "#1485D1"),
             toolbarTint: getColor("#5C6B8A", "#CCCCCC"),
             listSeparator: getColor("#D1D6E1", "#555555"),
-            listSelectedItemBackground: getColor("#F6F7F9", "#2A2E39"),
+            listSelectedItemBackground: getColor("#EDEDED", "#2A2E39"),
           }
         }
     };
-      
-      
-    
+
     const getSDK = () => {
         const sdk = Sumsub
             .init(token, tokenPromise)
